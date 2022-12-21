762da4ecc2dc0da74d5b9b843c70c534ddb080c5
Vitor da Costa Silva <vitordacostasilva@Vitors-MBP.lan>
2022-12-21 18:52:12 -0300

    Installation adjustments-2

--- install_zsh_ubuntu.sh
+++ install_zsh_ubuntu.sh
@@ -6,14 +6,14 @@ sudo apt-get update -y
 # install necessary packages/apps
 sudo apt-get install git curl wget sed -y
 
-# install ZSH shell
+# install ZSH shell, Oh-My-Zsh and Zsh-autosuggestions
 VAR_SHELL=$(echo $SHELL) 
 if [ "$VAR_SHELL" == "/bin/zsh" ]; then
-    echo "#####################\n"
+    echo -e "#####################\n"
     echo "ZSH já instalado e definido como padrao !!"
-    echo "#####################\n"
+    echo -e "#####################\n"
     
-    echo "### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
+    echo -e "### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
     #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P /tmp
     sed -i 's/exec zsh -l/#exec zsh -l/g' /tmp/install.sh
@@ -21,32 +21,24 @@ if [ "$VAR_SHELL" == "/bin/zsh" ]; then
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
-    echo "########################################### \n"
+    echo -e "\n###########################################\n"
     echo "Instalação concluída, REINICIAR A MÁQUINA"
-    echo "\n#########################################"
+    echo -e "#########################################\n"
 else
-    echo "### Instalacao Zsh shell em andamento... ###"
+    echo -e "\n### Instalacao Zsh shell em andamento... ###\n"
     zsh --version || sudo apt install zsh -y
     chsh -s /bin/zsh # Command use for set standard shell
     #zsh # enter ZSH shell
 
-    echo "### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
+    echo -e "\n### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
     #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
     wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P /tmp
     sed -i 's/exec zsh -l/#exec zsh -l/g' /tmp/install.sh
-    sh /tmp/install.sh
+    sh /tmp/install.sh -y
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
-    echo "########################################### \n"
+    echo -e "\n###########################################\n"
     echo "Instalação concluída, REINICIAR A MÁQUINA"
-    echo "\n#########################################"
+    echo -e "\n#########################################\n"
 fi
-
-
-
-
-
-
-
-
