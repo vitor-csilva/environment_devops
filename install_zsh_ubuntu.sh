184b7eb31f213903eec1edbc12cb5cfb99b522b9
Vitor da Costa Silva <vitordacostasilva@Vitors-MBP.lan>
2022-12-21 17:54:15 -0300

    Installation adjustments

--- install_zsh_ubuntu.sh
+++ install_zsh_ubuntu.sh
@@ -4,7 +4,7 @@
 sudo apt-get update -y
 
 # install necessary packages/apps
-sudo apt-get install git curl wget -y
+sudo apt-get install git curl wget sed -y
 
 # install ZSH shell
 VAR_SHELL=$(echo $SHELL) 
@@ -14,7 +14,10 @@ if [ "$VAR_SHELL" == "/bin/zsh" ]; then
     echo "#####################\n"
     
     echo "### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P /tmp
+    sed -i 's/exec zsh -l/#exec zsh -l/g' /tmp/install.sh
+    sh /tmp/install.sh
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
@@ -25,10 +28,13 @@ else
     echo "### Instalacao Zsh shell em andamento... ###"
     zsh --version || sudo apt install zsh -y
     chsh -s /bin/zsh # Command use for set standard shell
-    zsh # enter ZSH shell
+    #zsh # enter ZSH shell
 
     echo "### Iniciando Configuracao e instalação do Oh-My-Zsh ###\n"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P /tmp
+    sed -i 's/exec zsh -l/#exec zsh -l/g' /tmp/install.sh
+    sh /tmp/install.sh
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
