8f34b842a6c733114b42f8963074f8ee5f7b123b
Vitor da Costa Silva <vitordacostasilva@Vitors-MBP.lan>
2022-12-21 11:26:18 -0300

    install ZSH shell

---
+++ install_zsh_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# att repositories
+sudo apt-get update -y
+
+# install necessary packages/apps
+sudo apt-get install git curl wget -y
+
+# install ZSH shell
+sudo apt install zsh -y
+chsh -s /bin/zsh  # Command use for set standard shell
+zsh # enter ZSH shell
+
+
+
+
+
